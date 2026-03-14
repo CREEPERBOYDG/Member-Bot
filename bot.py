58e21ab4be9e6eee370d0a0e669e792270ca1fb7
CREEPERBOYDG <96713739+CREEPERBOYDG@users.noreply.github.com>
2026-03-14 17:58:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,8 +27,12 @@ USERS_FILE = "users.json"
 def load_users():
     if not os.path.exists(USERS_FILE):
         return {}
-    with open(USERS_FILE, "r") as f:
-        return json.load(f)
+
+    try:
+        with open(USERS_FILE, "r") as f:
+            return json.load(f)
+    except:
+        return {}
 
 def save_users(users):
     with open(USERS_FILE, "w") as f:
